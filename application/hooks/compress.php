2eae37bd80f3b407caf9c5fc8e31df594dd3ec69
Brian Jacobel <bjacobel@bowdoin.edu>
2013-03-25 15:19:53 -0400

    stopped the whitespace minimizer from devouring javascript

--- application/hooks/compress.php
+++ application/hooks/compress.php
@@ -4,21 +4,29 @@ function compress()
 {
     $CI =& get_instance();
     $buffer = $CI->output->get_output();
- 
-    $search = array(
-	'/\>[^\S ]+/s', 
-	'/[^\S ]+\</s', 
-	'/(\s)+/s', // shorten multiple whitespace sequences
-	'#(?://)?<!\[CDATA\[(.*?)(?://)?\]\]>#s' //leave CDATA alone
-	);
-    $replace = array(
-	'>',
-	'<',
-	'\\1',
-	"//&lt;![CDATA[\n".'\1'."\n//]]>"
-	);
 
-    $buffer = preg_replace($search, $replace, $buffer);
+    //written by a regex god
+    //http://stackoverflow.com/a/5324014/2178152
+    $re = '%# Collapse whitespace everywhere but in blacklisted elements.
+        (?>             # Match all whitespans other than single space.
+          [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+        | \s{2,}        # or two or more consecutive-any-whitespace.
+        ) # Note: The remaining regex consumes no text at all...
+        (?=             # Ensure we are not in a blacklist tag.
+          [^<]*+        # Either zero or more non-"<" {normal*}
+          (?:           # Begin {(special normal*)*} construct
+            <           # or a < starting a non-blacklist tag.
+            (?!/?(?:textarea|pre|script)\b)
+            [^<]*+      # more non-"<" {normal*}
+          )*+           # Finish "unrolling-the-loop"
+          (?:           # Begin alternation group.
+            <           # Either a blacklist start tag.
+            (?>textarea|pre|script)\b
+          | \z          # or end of file.
+          )             # End alternation group.
+        )  # If we made it here, we are not in a blacklist tag.
+        %Six';
+    $text = preg_replace($re, " ", $buffer);
 
     $CI->output->set_output($buffer);
     $CI->output->_display();
