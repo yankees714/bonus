230b26622beb134d3c9886ab0d6baa33aad613a4
Brian Jacobel <bjacobel@bowdoin.edu>
2013-03-27 12:40:41 -0400

    the Instapaper button is ugly and unbranded, but it works

--- application/views/export.php
+++ application/views/export.php
@@ -1,17 +1,12 @@
-<div class="rdbWrapper" data-show-read-now="1" data-show-read-later="1" data-show-send-to-kindle="0" data-show-print="0" data-show-email="0" data-orientation="0" data-version="1" data-bg-color="#ffffff">
-</div>
-<script type="text/javascript">(function() {var s = document.getElementsByTagName("script")[0],rdb = document.createElement("script"); rdb.type = "text/javascript"; rdb.async = true; rdb.src = document.location.protocol + "//www.readability.com/embed.js"; s.parentNode.insertBefore(rdb, s); })();</script>
+<!-- Readability -->
+<div class="rdbWrapper" data-show-read-now="1" data-show-read-later="1" data-show-send-to-kindle="0" data-show-print="0" data-show-email="0" data-orientation="0" data-version="1" data-bg-color="#ffffff"></div><script type="text/javascript">(function() {var s = document.getElementsByTagName("script")[0],rdb = document.createElement("script"); rdb.type = "text/javascript"; rdb.async = true; rdb.src = document.location.protocol + "//www.readability.com/embed.js"; s.parentNode.insertBefore(rdb, s); })();</script>
 
+<!-- Instapaper -->
+<div class="instaWrapper"><iframe border="0" scrolling="no" width="78" height="17" allowtransparency="true" frameborder="0" style="margin-bottom: -3px; z-index: 1338; border: 0px; background-color: transparent; overflow: hidden;" src="http://www.instapaper.com/e2?url=<?echo current_url();?>&title=<?echo $article->title;?>&description=<?echo $body->body;?>"></iframe></div>
 
-<!-- we have to make our own instapaper button because they don't provide one -->
-<div class="instaWrapper">
-  <script type="text/javascript">javascript:function iprl5(){var d=document,z=d.createElement('scr'+'ipt'),b=d.body,l=d.location;try{if(!b)throw(0);d.title='(Saving...) '+d.title;z.setAttribute('src',l.protocol+'//www.instapaper.com/j/3Kf0O6XBwYB0?u='+encodeURIComponent(l.href)+'&t='+(new Date().getTime()));b.appendChild(z);}catch(e){alert('Please wait until the page has loaded.');}}iprl5();void(0)</script>
-</div>
-
-
+<!-- Pocket -->
 <div class="pocketWrapper">
 </div>
 
-<div class="kindleWidget" style="display:inline-block;padding:3px;cursor:pointer;font-size:11px;font-family:Tahoma;white-space:nowrap;line-height:1;border-radius:3px;border:#ccc thin solid;color:black;background:transparent url('https://d1xnn692s7u6t6.cloudfront.net/button-gradient.png') repeat-x;background-size:contain;"><img style="vertical-align:middle;margin:0;padding:0;border:none;" src="https://d1xnn692s7u6t6.cloudfront.net/white-15.png" /><span style="vertical-align:middle;margin-left:3px;">Send to Kindle</span></div>
-<script type="text/javascript" src="https://d1xnn692s7u6t6.cloudfront.net/widget.js"></script>
-<script type="text/javascript">(function k(){window.$SendToKindle&&window.$SendToKindle.Widget?$SendToKindle.Widget.init({"content":"articlebody","title":"articletitle","author":"articleauthor","published":"articledate"}):setTimeout(k,500);})();</script>
+<!-- Kindle -->
+<div class="kindleWidget" style="display:inline-block;padding:3px;cursor:pointer;font-size:11px;font-family:Tahoma;white-space:nowrap;line-height:1;border-radius:3px;border:#ccc thin solid;color:black;background:transparent url('https://d1xnn692s7u6t6.cloudfront.net/button-gradient.png') repeat-x;background-size:contain;"><img style="vertical-align:middle;margin:0;padding:0;border:none;" src="https://d1xnn692s7u6t6.cloudfront.net/white-15.png" /><span style="vertical-align:middle;margin-left:3px;">Send to Kindle</span></div><script type="text/javascript" src="https://d1xnn692s7u6t6.cloudfront.net/widget.js"></script><script type="text/javascript">(function k(){window.$SendToKindle&&window.$SendToKindle.Widget?$SendToKindle.Widget.init({"content":"articlebody","title":"articletitle","author":"articleauthor","published":"articledate"}):setTimeout(k,500);})();</script>
