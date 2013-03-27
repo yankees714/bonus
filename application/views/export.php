3d65c77800ca6b47fe1eb52fecca7250e9e6e5b3
Brian Jacobel <bjacobel@bowdoin.edu>
2013-03-27 15:07:04 -0400

    pocket button is kind of working (using kindle button as foundation)

--- application/views/export.php
+++ application/views/export.php
@@ -5,10 +5,8 @@
 <div class="instaWrapper"><iframe border="0" scrolling="no" width="78" height="17" allowtransparency="true" frameborder="0" style="margin-bottom: -3px; z-index: 1338; border: 0px; background-color: transparent; overflow: hidden;" src="http://www.instapaper.com/e2?url=<?echo current_url();?>&title=<?echo $article->title;?>&description=<?echo $body->body;?>"></iframe></div>
 
 <!-- Pocket -->
-<div class="pocketWrapper">
-<?echo current_url();?>
-<?echo $article->title;?>
-</div>
+<div class="pocketWrapper" style="display:inline-block;padding:3px;cursor:pointer;font-size:11px;font-family:Tahoma;white-space:nowrap;line-height:1;border-radius:3px;border:#ccc thin solid;color:black;background:transparent url('https://d1xnn692s7u6t6.cloudfront.net/button-gradient.png') repeat-x;background-size:contain;"><img style="vertical-align:middle;margin:0;padding:0;border:none;" src="/img/pocket2.png"/>
+<a style="text-decoration:none;color:black;" href="http://getpocket.com/button/click?via=getpocket.com&url=<?echo current_url();?>&title=<?echo $article->title;?>&action=save&k=12685-e6eab987e68dbca19f50cf7a" title="Save to Pocket"><span style="vertical-align:middle;margin-left:3px;">Send to Pocket</span></a></div>
 
 <!-- Kindle -->
 <div class="kindleWidget" style="display:inline-block;padding:3px;cursor:pointer;font-size:11px;font-family:Tahoma;white-space:nowrap;line-height:1;border-radius:3px;border:#ccc thin solid;color:black;background:transparent url('https://d1xnn692s7u6t6.cloudfront.net/button-gradient.png') repeat-x;background-size:contain;"><img style="vertical-align:middle;margin:0;padding:0;border:none;" src="https://d1xnn692s7u6t6.cloudfront.net/white-15.png" /><span style="vertical-align:middle;margin-left:3px;">Send to Kindle</span></div><script type="text/javascript" src="https://d1xnn692s7u6t6.cloudfront.net/widget.js"></script><script type="text/javascript">(function k(){window.$SendToKindle&&window.$SendToKindle.Widget?$SendToKindle.Widget.init({"content":"articlebody","title":"articletitle","author":"articleauthor","published":"articledate"}):setTimeout(k,500);})();</script>
