bd2103f35236874d409c68f829421a4262e3ecad
Brian Jacobel <bjacobel@bowdoin.edu>
2013-03-28 11:24:23 -0400

    the buttons (functional as they are) are being loaded by javascript

--- application/views/article.php
+++ application/views/article.php
@@ -113,27 +113,18 @@
               var clickedid = $(this).attr("id");
               switch(clickedid){
                 case "readability":
-                     $('#addtoreader').replaceWith('');
+                     $('#addtoreader').replaceWith('<div id="readability" class="rdbWrapper readerEmbed" data-show-read-now="0" data-show-read-later="1" data-show-send-to-kindle="0" data-show-print="0" data-show-email="0" data-orientation="0" data-version="1" data-bg-color="#ffffff"></div><script type="text/javascript">(function() {var s = document.getElementsByTagName("script")[0],rdb = document.createElement("script"); rdb.type = "text/javascript"; rdb.async = true; rdb.src = document.location.protocol + "//www.readability.com/embed.js"; s.parentNode.insertBefore(rdb, s); })();</scr'+'ipt>'); //the hackiest hack that was ever hacked
                      break;
                  case "instapaper":
-                     $('#addtoreader').replaceWith('');
+                     $('#addtoreader').replaceWith('<div id="instapaper" class="readerEmbed"><iframe border="0" scrolling="no" width="78" height="17" allowtransparency="true" frameborder="0" style="margin-bottom: -3px; z-index: 1338; border: 0px; background-color: transparent; overflow: hidden;" src="http://www.instapaper.com/e2?url=<?echo current_url();?>&title=<?echo $article->title;?>&description=<?echo $body->body;?>"></iframe></div>');
                      break;
                 case "pocket":
-                     $('#addtoreader').replaceWith('');
+                     $('#addtoreader').replaceWith('<div id="pocket" class="readerEmbed" style="display:inline-block;padding:3px;cursor:pointer;font-size:11px;font-family:Tahoma;white-space:nowrap;line-height:1;border-radius:3px;border:#ccc thin solid;color:black;background:transparent url(' + 'https://d1xnn692s7u6t6.cloudfront.net/button-gradient.png' + ') repeat-x;background-size:contain;"><img style="vertical-align:middle;margin:0;padding:0;border:none;" src="/img/pocket2.png"/><a style="text-decoration:none;color:black;" href="http://getpocket.com/button/click?via=bowdoinorient.com&url=<?echo current_url();?>&title=<?echo $article->title;?>&action=save&k=12685-e6eab987e68dbca19f50cf7a" title="Save to Pocket"><span style="vertical-align:middle;margin-left:3px;">Send to Pocket</span></a></div>');
                      break;
                 case "kindle":
-                     $('#addtoreader').replaceWith('');
+                     $('#addtoreader').replaceWith('<div id="kindle" class="readerEmbed" style="display:inline-block;padding:3px;cursor:pointer;font-size:11px;font-family:Tahoma;white-space:nowrap;line-height:1;border-radius:3px;border:#ccc thin solid;color:black;background:transparent url(' +' https://d1xnn692s7u6t6.cloudfront.net/button-gradient.png' + ') repeat-x;background-size:contain;"><img style="vertical-align:middle;margin:0;padding:0;border:none;" src="https://d1xnn692s7u6t6.cloudfront.net/white-15.png" /><span style="vertical-align:middle;margin-left:3px;">Send to Kindle</span></div><script type="text/javascript" src="https://d1xnn692s7u6t6.cloudfront.net/widget.js"></scr' + 'ipt><script type="text/javascript">(function k(){window.$SendToKindle&&window.$SendToKindle.Widget?$SendToKindle.Widget.init({"content":"articlebody","title":"articletitle","author":"articleauthor","published":"articledate"}):setTimeout(k,500);})();</scr'+'ipt>');
                      break;
               }
-              /*$.ajax({
-                type: "POST",
-                url: "/application/controllers/readwidget.php",
-                data: {clickedid: clickedid},
-                success: function(data){alert(data);},
-                error: function(xhr, textStatus, error){
-                  alert(textStatus+" "+xhr+" "+error);
-                }
-              });*/
             });
             </script>
           </div>
