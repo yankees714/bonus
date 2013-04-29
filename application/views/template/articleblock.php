071dfa576a98270b3eb0d3cb0095bfbccd825792
Brian Jacobel <bjacobel@bowdoin.edu>
2013-04-29 10:56:39 -0400

    should have been using \!empty(), not isset()

--- application/views/template/articleblock.php
+++ application/views/template/articleblock.php
@@ -7,11 +7,11 @@ dateified: does it show a date
 medtile: is it medium-sized (body footer only, right now)
 dateoverlay: the "1 day ago" red overlay text -->
 
-<ul class="articleblock<?if(isset($twotier)&&$twotier):?> twotier<?endif;?><?if(isset($leftmargin)&&$leftmargin):?> leftmargin<?endif;?><?if(isset($rightmargin)&&$rightmargin):?> rightmargin<?endif;?>">
+<ul class="articleblock<?if(!empty($twotier)):?> twotier<?endif;?><?if(!empty($leftmargin)):?> leftmargin<?endif;?><?if(!empty($rightmargin)):?> rightmargin<?endif;?>">
 	<?foreach($blocks as $block):?>
-	<li class="<? if(!empty($block->filename_small)): ?> backgrounded<? endif; ?><? if(!$block->published): ?> draft<? endif; ?><? if(strtotime($date)-strtotime($block->date) > (7*24*60*60)): ?> old<? endif; ?><?if(isset($medtile)&&$medtile):?> medtile<?endif;?>"<? if(!empty($block->filename_small)): ?> style="background:url('<?=base_url().'images/'.$block->date.'/'.$block->filename_small?>')"<? endif; ?>>
+	<li class="<? if(!empty($block->filename_small)): ?> backgrounded<? endif; ?><? if(!$block->published): ?> draft<? endif; ?><? if(strtotime($date)-strtotime($block->date) > (7*24*60*60)): ?> old<? endif; ?><?if(!empty($medtile)):?> medtile<?endif;?>"<? if(!empty($block->filename_small)): ?> style="background:url('<?=base_url().'images/'.$block->date.'/'.$block->filename_small?>')"<? endif; ?>>
 		<a href="<?=site_url()?>article/<?=$block->id?>">
-			<?if(isset($dateified)):?>
+			<?if(!empty($dateified)):?>
 			<div class="dateified"><?=dateify($block->date, $date)?></div>
 			<?endif;?>
 			<h3><? if($block->series): ?><span class="series"><?=$block->series?>:</span> <? endif; ?><?=$block->title?></h3>
