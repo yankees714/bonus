04fecb2f94fa36213cc926da2ff6411fd128f5b8
Brian Jacobel <bjacobel@gmail.com>
2013-07-27 21:54:16 -0400

    pathnames in setup script were wrong

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ echo "Would you like to OVERWRITE your httpd.conf and php.ini with the files inc
 echo "This is really only advisable if you don't host any other sites on this machine."
 read -p "Do it? [y/n]: " yn
 case $yn in
- [Yy]*) echo "OK. I'm making backups, just in case." && sudo cp /etc/apache2/httpd.conf /etc/apache2/httpd-backup.conf && sudo cp httpd.conf /etc/apache2 && sudo cp /etc/php.ini /etc/php-backup.ini && sudo cp php.ini /etc/;;
+ [Yy]*) echo "OK. I'm making backups, just in case." && sudo cp /etc/apache2/httpd.conf /etc/apache2/httpd-backup.conf && sudo cp local-setup/httpd.conf /etc/apache2 && sudo cp /etc/php.ini /etc/php-backup.ini && sudo cp local-setup/php.ini /etc/;;
  * ) echo "OK, I won't. You're on your own to get that working!!";;
 esac
 
