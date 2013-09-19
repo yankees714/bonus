53d6376da8394255caf0a8bcf36721ad347b5abd
Brian Jacobel <bjacobel@gmail.com>
2013-09-19 15:27:52 -0400

    The eternal question of how we make dev and prod play nice.
    The previous solution (using --assume-unchanged) was a godawful hack,
    and when I wrote it I had no idea that it'd make switching branches a
    living hell. Now that we're branching out to running Orient code on three,
    possibly four machines, with Git newbies trying to navigate the waters, it's time
    for something a little less janky. I think this is a reasonably secure solution
    to database and server root settings, but it might not play nice with all Apache
    versions. We'll find out!

--- setup.sh
+++ setup.sh
@@ -1,33 +1,19 @@
 #!/bin/sh
 
-# Replace production setup files with local ones 
-cp local-setup/local-config/.htaccess .htaccess
-cp local-setup/local-config/config.php application/config/config.php
-cp local-setup/local-config/database.php application/config/database.php
-
-
-# Tell git not to track those files
-git update-index --assume-unchanged .htaccess
-git update-index --assume-unchanged application/config/config.php
-git update-index --assume-unchanged application/config/database.php
-
-
 # Create an Orient database
 mysql -uroot -e "CREATE DATABASE DB02Orient;"
-
 
 # Get Apache and PHP set up
 echo "Would you like to OVERWRITE your httpd.conf and php.ini with the files included?"
 echo "This is really only advisable if you don't host any other sites on this machine."
 read -p "Do it? [y/n]: " yn
 case $yn in
- [Yy]*) echo "OK. I'm making backups, just in case." && sudo cp /etc/apache2/httpd.conf /etc/apache2/httpd-backup.conf && sudo cp local-setup/httpd.conf /etc/apache2 && sudo cp /etc/php.ini /etc/php-backup.ini && sudo cp local-setup/php.ini /etc/;;
+ [Yy]*) echo "OK. I'm making backups, just in case." && sudo cp /etc/apache2/httpd.conf /etc/apache2/httpd-backup.conf && sudo cp setup-files/httpd.conf /etc/apache2 && sudo cp /etc/php.ini /etc/php-backup.ini && sudo cp setup-files/php.ini /etc/;;
  * ) echo "OK, I won't. You're on your own to get that working!!";;
 esac
 
-
 # Make an apache vsite
-sudo cp ./local-setup/bowdoinorient.conf /etc/apache2/other
+sudo cp ./setup-files/bowdoinorient.conf /etc/apache2/other
 sudo ln -s `pwd` /Library/WebServer/Documents
 
 # make bowdoinorient.dev work
