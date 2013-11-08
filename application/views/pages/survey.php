11f81e7e400d9ba5d945c262cd6b2a541fa2b257
Brian Jacobel <bjacobel@gmail.com>
2013-11-07 22:01:12 -0500

    Putting in some blank space on survey page

--- application/views/pages/survey.php
+++ application/views/pages/survey.php
@@ -8,13 +8,15 @@
 	
 	<article id="mainstory">
 		
-		<!--<header>
+		<header>
 			<hgroup>
-				<h2 id="articletitle" class="articletitle">Survey</h2>
+				<h2 id="articletitle" class="articletitle">This survey's response period has closed.</h2>
 			</hgroup>			
-		</header>-->
+		</header>
 		
 		<div id="articlebody" class="articlebody">
+			<!-- make a big div fill up some space -->
+			<div style="display:block;height:80%"></div>
 			
 			<!-- <iframe src="https://docs.google.com/forms/d/16d4vIBYEQVhBer9bRuzd_yMaCa8fxJsoL5vCsS5cwyk/viewform?embedded=true" width="760" height="4000" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe> -->
 					
