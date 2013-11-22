a29c901777761df2e6b061eff6aad1bdbd8973d9
Brian Jacobel <bjacobel@gmail.com>
2013-11-22 00:57:08 -0500

    OARS 2013

--- application/views/pages/survey.php
+++ application/views/pages/survey.php
@@ -10,16 +10,15 @@
 		
 		<header>
 			<hgroup>
-				<h2 id="articletitle" class="articletitle">This survey's response period has closed.</h2>
+				<!-- <h2 id="articletitle" class="articletitle">This survey's response period has closed.</h2> -->
 			</hgroup>			
 		</header>
 		
 		<div id="articlebody" class="articlebody">
 			<!-- make a big div fill up some space -->
-			<div style="display:block;height:500px;width:1px;"></div>
+			<!-- <div style="display:block;height:500px;width:1px;"></div> -->
 			
-			<!-- <iframe src="https://docs.google.com/forms/d/16d4vIBYEQVhBer9bRuzd_yMaCa8fxJsoL5vCsS5cwyk/viewform?embedded=true" width="760" height="4000" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe> -->
-					
+		<iframe src="https://docs.google.com/forms/d/13f9kG6Z-GrKuoxuxl-KN-vxZ-HVvwH4vIECFa1lhIHY/viewform?embedded=true" width="760" height="2200" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>					
 		</div>
 	  
 	</article>
