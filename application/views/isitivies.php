96d871cb18cda9507e04a04e2a471d71b0d0ab23
Andrew Daniels <andrew.daniels714@gmail.com>
2015-04-21 16:16:28 -0400

    Remove the link to the ivies app which I don't think exists anymore?

--- application/views/isitivies.php
+++ application/views/isitivies.php
@@ -51,8 +51,6 @@ p {
 <p><a href="http://bowdoinorientexpress.com/post/21582257886/welcome-to-ivies-2012-5-rules-to-live-by">Five rules to live by (2012)</a></p>
 <p><a href="http://bowdoinorient.com/article/1821">Your guide to surviving Ivies (2006)</a></p>
 
-<a href="https://itunes.apple.com/us/app/iviescompanion/id634953896"><img src="<?=base_url()?>img/icon-app-store.svg" class="app-store-icon"></a>
-
 </body>
 
 </html>
